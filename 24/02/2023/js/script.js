5b66c924f651d46fee84c1e8a9435a99330d9c2b
EvertonYamata <119260451+EvertonYamata@users.noreply.github.com>
2023-02-24 22:03:42 -0300

    novo resultado final

--- 24/02/2023/js/script.js
+++ 24/02/2023/js/script.js
@@ -1,9 +1,11 @@
 const a = Number(prompt("digite o valor do seu salario"))
 const b = Number(prompt("digite a porcentagem de aumento de seu salario"))
 
-let resultado = a*b/100
+let resultado = b/100
 
 console.log(resultado)
+
+let resultadofinal = a * resultado
 
 document.write("seu salario passara a ser "+resultado)
 //document.write(resultado)
