2b7fbfa874edac52dab8d2793b500b6ade493873
EvertonYamata <119260451+EvertonYamata@users.noreply.github.com>
2023-02-24 22:11:37 -0300

    12123322343553425

--- 24/02/2023/js/script.js
+++ 24/02/2023/js/script.js
@@ -5,7 +5,7 @@ let resultado = b/100
 
 console.log(resultado)
 
-let resultadofinal = a * resultado
+let resultadofinal = a * (1+ resultado)
 console.log(resultadofinal)
 
 document.write("seu salario passara a ser "+resultadofinal)
