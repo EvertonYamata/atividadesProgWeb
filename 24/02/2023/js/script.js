f46d2514fb9c677601e7e374a52f010df471359b
EvertonYamata <119260451+EvertonYamata@users.noreply.github.com>
2023-02-24 21:44:47 -0300

    consulta de salario

---
+++ 24/02/2023/js/script.js
@@ -0,0 +1,8 @@
+const a = Number(prompt("digite o valor do seu salario"))
+const b = Number(prompt("digite a porcentagem de aumento de seu salario"))
+
+let resultado = a*b
+console.log(resultado)
+
+document.write("seu salario passara a ser")
+document.write(resultado)
