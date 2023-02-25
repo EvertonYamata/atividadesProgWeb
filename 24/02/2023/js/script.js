f1b2817f2b7f927bc4f70bb24f1c6d690f167b71
EvertonYamata <119260451+EvertonYamata@users.noreply.github.com>
2023-02-24 21:56:08 -0300

    ajuste na porcentagem

--- 24/02/2023/js/script.js
+++ 24/02/2023/js/script.js
@@ -1,7 +1,7 @@
 const a = Number(prompt("digite o valor do seu salario"))
 const b = Number(prompt("digite a porcentagem de aumento de seu salario"))
 
-let resultado = a*b
+let resultado = a*(b/10)
 console.log(resultado)
 
 document.write("seu salario passara a ser "+resultado)
